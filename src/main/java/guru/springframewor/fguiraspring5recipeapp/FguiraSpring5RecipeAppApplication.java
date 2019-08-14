6506e2040d1bb7541c16add92d6e08c2a0a53fc5
Francisco Guiraldelli <francisco.guiraldelli.com>
2019-08-14 12:59:25 -0300

    first commit

---
+++ src/main/java/guru/springframewor/fguiraspring5recipeapp/FguiraSpring5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframewor.fguiraspring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FguiraSpring5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FguiraSpring5RecipeAppApplication.class, args);
+    }
+
+}
